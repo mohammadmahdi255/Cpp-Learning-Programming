a99646d3626f049a41e774ca0b814a65572eb703
Mohammad Mahdi Nemati <nemati.mahdi255@gmail.com>
2023-02-03 09:12:19 +0330

    finish basic of c++

---
+++ Fundamentals/01. Basics/03. Naming Conventions/main.cpp
@@ -0,0 +1,7 @@
+int main() {
+    int file_size; // Snake Case
+    int FileSize;  // Pascal Case
+    int fileSize;  // Camel Case
+    int iFileSize; // Hungarian Notation
+    return 0;
+}
---
+++ Fundamentals/01. Basics/05. Order of Operators/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // ()
+    // * and /
+    // + and -
+    double x = 10;
+    double y = 5;
+    double z = (x + 10) / (3 * y);
+    std::cout << z << std::endl;
+
+    return EXIT_SUCCESS;
+}
